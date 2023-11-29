c65370c13b98f0b5c9cf47a04121d9359d0f6aba
rppp <88567952@qq.com>
2023-11-29 10:41:47 +0800

    Add files via upload

---
+++ full.py
@@ -0,0 +1,12 @@
+
+s='123fewwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww254341t4grgraedgregregregaregewgregreagrgarg\n'
+s = s+s
+s = s+s
+s = s+s
+s = s+s
+s = s+s
+s = s+s
+s = s+s
+with open('test123.txt', 'w') as f:
+    while True:
+        f.write(s)
