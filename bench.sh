94a2d8701646f85764d1a784854179749be51364
rppp <88567952@qq.com>
2024-09-04 13:10:00 +0800

    fix

--- bench.sh
+++ bench.sh
@@ -1,1 +1,2 @@
-g++ fib43.cpp -o fib43 -O2 && time ./fib43
+#g++ fib43.cpp -o fib43 -O2 && time ./fib43
+g++ fib43.s -o fib43.bin && time ./fib43.bin
---
+++ fib43.s
@@ -0,0 +1,92 @@
+	.file	"fib43.cpp"
+	.text
+	.globl	_Z3fibi
+	.type	_Z3fibi, @function
+_Z3fibi:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	pushq	%rbx
+	subq	$24, %rsp
+	.cfi_offset 3, -24
+	movl	%edi, -20(%rbp)
+	cmpl	$1, -20(%rbp)
+	jg	.L2
+	movl	$1, %eax
+	jmp	.L3
+.L2:
+	movl	-20(%rbp), %eax
+	subl	$1, %eax
+	movl	%eax, %edi
+	call	_Z3fibi
+	movl	%eax, %ebx
+	movl	-20(%rbp), %eax
+	subl	$2, %eax
+	movl	%eax, %edi
+	call	_Z3fibi
+	addl	%ebx, %eax
+.L3:
+	movq	-8(%rbp), %rbx
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	_Z3fibi, .-_Z3fibi
+	.section	.rodata
+.LC0:
+	.string	"%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$8, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$43, %edi
+	call	_Z3fibi
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
