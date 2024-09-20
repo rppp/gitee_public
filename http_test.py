833bf878bde9665fa1a21216c90edef8dc9ae688
rppp <88567952@qq.com>
2024-09-21 02:41:19 +0800

    fix

--- http_test.py
+++ http_test.py
@@ -3,13 +3,39 @@ from multiprocessing import Pool, cpu_count
 import time
 import statistics
 
+g_s = '''
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to nginx!</title>
+<style>
+html { color-scheme: light dark; }
+body { width: 35em; margin: 0 auto;
+font-family: Tahoma, Verdana, Arial, sans-serif; }
+</style>
+</head>
+<body>
+<h1>Welcome to nginx!</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>
+'''
+
 def single_request(url):
     start_time = time.time()
     try:
         response = requests.get(url, timeout=2)
         end_time = time.time()
         delay = end_time - start_time
-        return (0 if response.status_code == 200 else 1, delay)
+        return (0 if response.status_code == 200 and response.text.strip() == g_s.strip() else 1, delay)
     except requests.RequestException:
         end_time = time.time()
         delay = end_time - start_time
@@ -26,7 +52,7 @@ def main():
     print("开始网络测试...")
     
     url = "http://b.rppp.ren/" #发现用500次，5g郴州会丢包，但电信宽带不会
-    count = 1500
+    count = 3000*3*60*4 #3000次大约21秒
     
     print(f"\n测试失败率和延迟... URL: {url}, 次数: {count}")
     start_time = time.time()
